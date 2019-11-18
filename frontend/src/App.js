f9bb7b61dfe026deddacbbb8c00667f47a40f3f7
Ishan (2016831017) <ahsan17@student.sust.edu>
2019-11-18 22:49:30 +0600

    Post Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Axios from "axios";
+import Post from "./components/Post/Post";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    Axios.get("/api/v1/posts").then(response => {
+      this.setState({ posts: response.data });
+    });
+  }
+  render() {
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+
+        <div>
+          {this.state.posts.map((post, id) => (
+            <Post data={post} key={id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import Axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Axios.defaults.baseURL = "http://localhost:7000/";
+
+Axios.interceptors.request.use(function(config) {
+  const token = localStorage.getItem("token");
+  config.headers.authorization = token;
+
+  return config;
+});
+
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
